6af9673064ad17aeede3a8bd56df24e2b388793d
Wenying Chang <changwenyingnl@gmail.com>
2024-03-29 18:27:31 +0100

    improve log in page

--- src/Components/Account/Account.tsx
+++ src/Components/Account/Account.tsx
@@ -4,6 +4,8 @@ import { TextField, Button } from '@mui/material';
 import './Account.css';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
+import validator from 'validator';
+import { IoAlertCircle } from "react-icons/io5";
 
 
 const firebaseConfig = {
@@ -25,12 +27,17 @@ const auth = app.auth();
 const Account: React.FC = () => {
 
     const [email, setEmail] = useState<string>('');
+    const [isEmailValid, setIsEmailValid] = useState<boolean>(false);
+    const [emailInvalidMessage, setEmailInvalidMessage] =useState<string>('');
     const [password, setPassword] = useState<string>('');
     const [signedIn, setSignedIn] = useState<boolean>(false);
     const [userName, setUserName] = useState<string>('');
+    const [isAccountFormValid, setIsAccountFormValid] = useState<boolean>(false);
+    const [signInErrorMessage, setSignInErrorMessage] = useState<string | null>(null);
 
     //check if user is signed in from session Storage or local storage
     useEffect(() => {
+        
         const isSignedIn = sessionStorage.getItem('signedIn') === 'true';
         setSignedIn(isSignedIn);
         if (isSignedIn) {
@@ -38,6 +45,33 @@ const Account: React.FC = () => {
             setEmail(sessionStorage.getItem('email') || '');
         }
     }, []);
+
+    const validateEmail = (email: string) => {
+         if(validator.isEmail(email)){
+            setIsEmailValid(true);
+            setEmailInvalidMessage('');  
+        } else{
+            setIsEmailValid(false);
+            setEmailInvalidMessage('Please enter a valid email address');
+        }
+        setIsAccountFormValid(userName !== "" && password !== "");
+    };
+
+    useEffect(()=>{
+        //update the form validability when any input changes
+        setIsAccountFormValid(userName !== "" && password !== "");
+    },[userName, email, password]);
+
+    const handleSignUp = async () => {
+        try{
+            //create new user via firebase
+            const userCredential = await auth.createUserWithEmailAndPassword(email, password);
+            //update sign in state after successfully sign up
+            setSignedIn(true);
+        } catch (error: any) {
+            console.error('Error signing up:', error.message);
+          }
+    };
 
     //function to handle sign in 
     const handleSignIn = async () : Promise<void> => {
@@ -50,7 +84,9 @@ const Account: React.FC = () => {
             sessionStorage.setItem('userName', userName);
             sessionStorage.setItem('email', email);
           } catch (error: any) {
-            console.error('Error signing in:', error.message);
+            setSignInErrorMessage("Invalid email or password")
+          } finally {
+            setIsAccountFormValid(true);
           }
     };
 
@@ -75,11 +111,18 @@ const Account: React.FC = () => {
     const signInForm = () : JSX.Element => (
         <div className="account-content">
             <TextField required id="UserName" label="User Name" variant="outlined" value={userName} onChange={(e) => {setUserName(e.target.value)}}/>
-            <TextField required id="Email" label="Email" type="email" variant="outlined" value={email} onChange={(e) => {setEmail(e.target.value)}}/>
+            <TextField required id="Email" label="Email" type="email" variant="outlined" value={email} onChange={(e) => {setEmail(e.target.value)}} onBlur={ () => validateEmail(email) } error={!isEmailValid} helperText={isEmailValid ? '' : `${emailInvalidMessage}`}/>
             <TextField required id="Password" label="Password" type="password"variant="outlined" value={password} onChange={(e) => {setPassword(e.target.value)}}/>
+            {signInErrorMessage && (
+                <div className="error-message">
+                    <IoAlertCircle style={{ width:'20px', height: '20px', color:'#E32327' }}/>
+                    <span>{signInErrorMessage}</span>
+                </div>
+            )}
             <div className="buttons">
-                
-                <Button variant="contained" onClick={handleSignIn}>Sign in</Button>
+                <Button variant="outlined" onClick={handleSignUp} disabled={!isAccountFormValid}>Sign up</Button>
+                <Button variant="contained" onClick={handleSignIn} disabled={!isAccountFormValid}>Sign in</Button>
+
             </div>
             
         </div>
