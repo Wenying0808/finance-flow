b32d1335086ca6aa99bc9dfd8a3c9961782f0203
Wenying Chang <changwenyingnl@gmail.com>
2024-07-30 15:47:39 +0200

    feat:// access the sum of each category

--- src/Components/DataChart/MonthlyDataChart.tsx
+++ src/Components/DataChart/MonthlyDataChart.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "./MonthlyDataChart.css";
 import { Expense } from "../Expenses/ExpenseInterface";
-
 import { Categories } from "../Expenses/Categories";
 
 interface monthylyDataChartProps {
@@ -9,6 +8,22 @@ interface monthylyDataChartProps {
     currencySymbol: string;
 };
 const MonthlyDataChart: React.FC<monthylyDataChartProps> = ( {expenses, currencySymbol} ) => {
+
+    // transform expenses data to chart data :[{category: 'Education', amount: 100}, {category: 'Education', amount: 100}]
+    const chartData = expenses;
+    console.log(chartData);
+    
+    // get each category value from the categories object
+    const categoryValues = Categories.map(category => category.value);
+
+    // find the total amount for each category
+    const sumOfEachCategory = categoryValues.reduce((dataObject, category) => {
+        dataObject[category] = expenses.filter(expense => expense.category === category).reduce((sum, expense) => sum + expense.amount, 0);
+        return dataObject;
+    }, {} as Record<string, number>);
+
+    console.log(sumOfEachCategory);
+    console.log(typeof sumOfEachCategory);
 
     return(
         <div className="monthly-data-chart">
